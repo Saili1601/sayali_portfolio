00f9dc678a24dc189e4d6ee8e8c759dcc86add10
Saili1601 <sailithombare@gmail.com>
2025-05-07 22:37:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
+import { Projects } from "./components/sections/Projects";
+import { ContactForm } from "./components/sections/ContactForm";
+import "./index.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen bg-black text-gray-100 transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
+          }`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
+        <Projects />
+        <ContactForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
