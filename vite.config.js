743e5b7e948a6dc7ec77b985ef0d3d028bf5cb0f
Saili1601 <sailithombare@gmail.com>
2025-09-17 15:34:50 +0530

    fix it

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Sailicodes-portfolio",
+  build: {
+    outDir: "dist",
+  },
+  base: "/Sailicodes-portfolio/",
 });
